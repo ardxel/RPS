4ba3aca06844339ccb2b7ad442e7447a0fe71278
ardxel <ardxel@gmail.com>
2022-11-27 04:48:06 +0300

    add draw window

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { start } from './script.js'
 
-let startButton = document.querySelector('input[value=start]');
+const startButton = document.querySelector('input[value=start]');
+
 startButton.addEventListener('click', start);
+
--- script.js
+++ script.js
@@ -1,23 +1,85 @@
 
 export function start() {
+    let panel = document.getElementById('button-panel');
+    let buttons = document.querySelectorAll('.open-button');
 
-    let buttons = document.querySelectorAll('input[type=button]');
     for (let button of buttons) {
         button.style.display = 'none';
     }
-    let panel = document.getElementById('button-panel');
 
     for (let i = 1; i <= 3; i++) {
         let input = document.createElement('input');
-        if (i == 1) input.value = 'камень';
-        if (i == 2) input.value = 'ножницы';
-        if (i == 3) input.value = 'бумага';
-
         input.type = 'button';
+        input.className = 'game-input'
+    
+        switch(i) {
+            case 1: input.value = 'rock';
+                break;
+            case 2: input.value = 'scissors';
+                break;
+            case 3: input.value = 'paper';
+                break;
+        }
         
+        input.addEventListener('click', function() {
+            const answerAI = generator();
+            const answer = input.value;
+            
+            whoHasWon(answer, answerAI);
+
+            for (let button of document.querySelectorAll('.game-input')) {
+                button.style.display = 'none';
+            }
+            for (let openButton of document.querySelectorAll('.open-button')) {
+                openButton.style.display = '';
+            }
+        })
+
         panel.append(input);
     }
 
     let startMessage = document.querySelector('.start-message');
-    startMessage.remove();
+    startMessage.style.display = 'none';
+
 }
+
+function generator() {
+    const obj = {
+        1: 'rock',
+        2: 'scissors',
+        3: 'paper'
+    }
+
+    const randomNum = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    
+    return obj[randomNum];
+}
+
+function whoHasWon(p1, p2) {
+    if (p1 == p2) return showDraw();
+
+    const rules = {rock: 'scissors', paper: 'rock', scissors: 'paper'}
+    //first player won
+    if (p2 == rules[p1]) {
+        
+    }
+    //computer won
+    else {
+
+    }
+}
+
+function showDraw() {
+    const board  = document.getElementById('board-event');
+    
+    if (board.childNodes.length) {
+        board.innerHTML = '';
+    }
+
+    const drawMessage = document.createElement('span');
+    drawMessage.className = 'draw';
+    drawMessage.innerHTML = 'Произошла ничья';
+
+    board.append(drawMessage);
+}
+
