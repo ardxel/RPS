56a884cc3dcf9a76f9b18ef227165f777be07a10
ardxel <ardxel@gmail.com>
2022-11-27 14:46:42 +0300

    bug fix and add new button RESTART

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { start } from './script.js'
+import { restart } from './script.js'
 
 const startButton = document.querySelector('input[value=start]');
+const restartButton = document.querySelector('input[value=restart');
 
 startButton.addEventListener('click', start);
-
+restartButton.addEventListener('click', restart);
