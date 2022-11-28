3820e0fb0ca94b1ccc8d0937f31148551efc8693
ardxel <ardxel@gmail.com>
2022-11-28 12:07:04 +0300

    history button functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var historyData = [];
 
 export function start() {
     let panel = document.getElementById('button-panel');
@@ -75,11 +76,23 @@ function generator() {
 }
 
 function whoHasWon(p1, p2) {
-    if (p1 == p2) return showDraw();
-
     const rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' }
+    let result;
 
-    p2 == rules[p1] ? winner('player-human') : winner('player-AI');
+    if (p1 == p2) {
+        result = 'Draw'
+        showDraw();
+    }
+
+    else if (p2 == rules[p1]) {
+        result = 'player-human';
+        winner('player-human');
+    } else {
+        result = 'player-AI';
+        winner('player-AI');
+    }
+
+    addLiElement(p1, p2, result, new Date());
 }
 
 function showDraw() {
@@ -128,8 +141,37 @@ export function restart() {
     document.querySelectorAll('.score').forEach(score => {
         score.innerHTML = 0;
     })
+    document.querySelector('.start-message').style.display = '';
+}
 
 
+// history event
+const historyMain = document.querySelector('#main-history');
+export function history() { }
+
+function addLiElement(answer1, answer2, result, date) {
+    const answers = `you: ${answer1}, AI: ${answer2}`;
+    const verdict = (result) => {
+        if (result == 'Draw') {
+            return 'Draw';
+        } else return `winner: ${result}`;
+    };
+    const eventDate = strDate(date);
+
+    let li = document.createElement('li');
+    li.innerHTML = `${answers}, ${verdict(result)} [${eventDate}]`;
+    historyMain.firstElementChild.prepend(li);
+}
 
 
+function strDate(date) {
+    let year = String(date.getFullYear()).slice(2);
+    let mon = date.getMonth() + 1;
+    let days = date.getDate();
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    return `${h}:${m}:${s} ${days}:${mon}:${year}`;
 }
